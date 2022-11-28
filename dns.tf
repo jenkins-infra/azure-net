61913263e136e4be2ad7f80820fecff969bae9e7
Hervé Le Meur <hlemeur@cloudbees.com>
2022-11-28 11:42:43 +0100

    missing files for cloudinit

---
+++ dns.tf
@@ -0,0 +1,9 @@
+# TODO: import as resource
+data "azurerm_resource_group" "proddns_jenkinsio" {
+  name = "proddns_jenkinsio"
+}
+
+# TODO: import as resource
+data "azurerm_dns_zone" "jenkinsio" {
+  name = "jenkins.io"
+}
