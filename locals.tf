0d61ca7961735bfb90aee31c8bf9e2a26ec22518
Hervé Le Meur <hlemeur@cloudbees.com>
2022-11-30 18:01:17 +0100

    fmt

--- locals.tf
+++ locals.tf
@@ -8,11 +8,11 @@ locals {
     shorthostname = "vpn-test"
     username      = "jenkins-infra-team"
     allowed_ips = {
-      dduportal     = "109.88.253.68/32"
-      lemeurherve   = "176.185.227.180/32"
-      smerle33      = "82.64.5.129/32"
-      olblak        = "109.128.249.199/32"
-      olblak_bis    = "86.130.79.46/32"
+      dduportal   = "109.88.253.68/32"
+      lemeurherve = "176.185.227.180/32"
+      smerle33    = "82.64.5.129/32"
+      olblak      = "109.128.249.199/32"
+      olblak_bis  = "86.130.79.46/32"
     }
     puppet_ips = ["140.211.9.94"]
   }
