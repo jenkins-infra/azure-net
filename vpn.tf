b5c6d26273f6371726e8b29f3bb9a0e3b105c9bf
Hervé Le Meur <hlemeur@cloudbees.com>
2022-11-30 16:42:28 +0100

    fix: tfsec ignore on puppet-outbound rule

--- vpn.tf
+++ vpn.tf
@@ -107,6 +107,7 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  #tfsec:ignore:azure-network-no-public-egress
   security_rule {
     name                       = "allow-puppet-outbound"
     priority                   = 2002
