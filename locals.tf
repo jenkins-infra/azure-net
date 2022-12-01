275a081782b283f609faa9ea2d6bb5f7ee832bf6
Hervé Le Meur <hlemeur@cloudbees.com>
2022-12-01 10:38:45 +0100

    fix: less confusing local variable names

--- locals.tf
+++ locals.tf
@@ -7,12 +7,12 @@ locals {
   vpn = {
     shorthostname = "vpn-test"
     username      = "jenkins-infra-team"
-    allowed_ips = {
+    ssh_allowed_inbound_ips = {
       dduportal   = "109.88.253.68/32"
       lemeurherve = "176.185.227.180/32"
       smerle33    = "82.64.5.129/32"
     }
-    puppet_ips = {
+    puppet_outbound_ips = {
       # dig puppet.jenkins.io
       puppet_jenkins_io = "140.211.9.94"
     }
