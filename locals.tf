7b231853bf424fdffe8a544324dfc54ed985c2e1
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-12-01 10:12:28 +0100

    Apply suggestions from code review
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- locals.tf
+++ locals.tf
@@ -11,12 +11,10 @@ locals {
       dduportal   = "109.88.253.68/32"
       lemeurherve = "176.185.227.180/32"
       smerle33    = "82.64.5.129/32"
-      olblak      = "109.128.249.199/32"
-      olblak_bis  = "86.130.79.46/32"
     }
     puppet_ips = {
       # dig puppet.jenkins.io
-      radish_jenkins_io = "140.211.9.94"
+      puppet_jenkins_io = "140.211.9.94"
     }
   }
 }
