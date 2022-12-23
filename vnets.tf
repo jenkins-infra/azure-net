889056785314f3bf63307ef77e7869a4c8767f52
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-12-23 12:27:59 +0100

    Update vnets.tf

--- vnets.tf
+++ vnets.tf
@@ -64,7 +64,7 @@ resource "azurerm_subnet" "dmz" {
 }
 
 # Dedicated subnet for machine to machine private communications
-resource "azurerm_subnet" "data_tier" {
+resource "azurerm_subnet" "private_vnet_data_tier" {
   name                 = "${azurerm_virtual_network.private.name}-data-tier"
   resource_group_name  = azurerm_resource_group.private.name
   virtual_network_name = azurerm_virtual_network.private.name
