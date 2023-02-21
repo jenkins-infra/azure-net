e0823a9a69313db9c4f58c4f5cd375623b175781
Damien Duportal <damien.duportal@gmail.com>
2023-02-21 14:20:25 +0100

    hotfix(locals) add dduportal second public IP

--- locals.tf
+++ locals.tf
@@ -9,6 +9,7 @@ locals {
     username      = "jenkins-infra-team"
     ssh_allowed_inbound_ips = {
       dduportal   = "109.88.253.125/32"
+      dduportal2  = "149.154.214.236/32"
       lemeurherve = "176.185.227.180/32"
       smerle33    = "82.64.5.129/32"
       mwaite      = "162.142.59.220/32"
