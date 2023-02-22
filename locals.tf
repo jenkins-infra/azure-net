385856ff8c7bc61c298f3c32ebe6b8587a1ba603
Damien Duportal <damien.duportal@gmail.com>
2023-02-22 08:47:57 +0100

    fixup

--- locals.tf
+++ locals.tf
@@ -9,10 +9,10 @@ locals {
     username      = "jenkins-infra-team"
     ssh_allowed_inbound_ips = {
       dduportal   = "85.27.58.68/32"
-      dduportal2  = "149.154.214.236/32"
       lemeurherve = "176.185.227.180/32"
       smerle33    = "82.64.5.129/32"
       mwaite      = "162.142.59.220/32"
+      dduportal2  = "149.154.214.236/32"
     }
     puppet_outbound_ips = {
       # dig puppet.jenkins.io
