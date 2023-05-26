799883b610cf4941bbcaecb0a5f4e81567c38fc7
Damien Duportal <damien.duportal@gmail.com>
2023-05-26 17:28:43 +0200

    feat(dns) import jenkins-ci.org records already migrated to publick8s (#86)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- dns.tf
+++ dns.tf
@@ -3,6 +3,11 @@ data "azurerm_resource_group" "proddns_jenkinsio" {
   name = "proddns_jenkinsio"
 }
 
+# TODO: import as resource
+data "azurerm_resource_group" "proddns_jenkinsci" {
+  name = "proddns_jenkinsci"
+}
+
 resource "azurerm_resource_group" "proddns_jenkinsisthewayio" {
   name     = "proddns_jenkinsisthewayio"
   location = "East US 2"
@@ -12,6 +17,12 @@ resource "azurerm_resource_group" "proddns_jenkinsisthewayio" {
 data "azurerm_dns_zone" "jenkinsio" {
   name                = "jenkins.io"
   resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
+}
+
+# TODO: import as resource
+data "azurerm_dns_zone" "jenkinsciorg" {
+  name                = "jenkins-ci.org"
+  resource_group_name = data.azurerm_resource_group.proddns_jenkinsci.name
 }
 
 resource "azurerm_dns_zone" "jenkinsistheway_io" {
