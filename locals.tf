1acb8c5052cec7484c5d75826d9bd03e70c6810c
Damien Duportal <damien.duportal@gmail.com>
2023-09-17 11:16:10 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -10,9 +10,8 @@ locals {
     ssh_allowed_inbound_ips = {
       dduportal = {
         ips = [
-          "85.27.34.43/32",   # Home
-          "91.182.56.152/32", # Cowork
-          "91.175.63.234/32", # Cowork-2
+          "85.27.34.43/32",
+          "86.202.255.126/32",
         ],
         priority = 101,
       },
