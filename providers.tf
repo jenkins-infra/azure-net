0021f5b37282be7bb66736dc397438396ec417d3
Damien Duportal <damien.duportal@gmail.com>
2023-11-16 18:55:48 +0100

    feat: add the public vnet for the secondary (sponsored) subscription
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- providers.tf
+++ providers.tf
@@ -1,5 +1,11 @@
-# Configure the Microsoft Azure Provider
+# Configure the Microsoft Azure Providers
 provider "azurerm" {
+  skip_provider_registration = "true"
+  features {}
+}
+provider "azurerm" {
+  alias                      = "jenkins-sponsorship"
+  subscription_id            = "1311c09f-aee0-4d6c-99a4-392c2b543204"
   skip_provider_registration = "true"
   features {}
 }
