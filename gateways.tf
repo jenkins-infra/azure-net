dbda26a01fe21d8dd19367975a8be53483583a3d
Damien Duportal <damien.duportal@gmail.com>
2024-01-05 21:15:52 +0100

    feat(gateways) migrate to module (#188)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- gateways.tf
+++ gateways.tf
@@ -1,60 +1,73 @@
 ####################################################################################
 ## NAT gateway to allow outbound connection on a centralized and scalable appliance
 ####################################################################################
-resource "azurerm_public_ip" "cert_ci_jenkins_io_outbound" {
+moved {
+  from = azurerm_public_ip.cert_ci_jenkins_io_outbound
+  to   = module.cert_ci_jenkins_io_outbound.azurerm_public_ip.outbound
+}
+moved {
+  from = azurerm_nat_gateway.cert_ci_jenkins_io_outbound
+  to   = module.cert_ci_jenkins_io_outbound.azurerm_nat_gateway.outbound
+}
+moved {
+  from = azurerm_nat_gateway_public_ip_association.cert_ci_jenkins_io_outbound
+  to   = module.cert_ci_jenkins_io_outbound.azurerm_nat_gateway_public_ip_association.outbound
+}
+moved {
+  from = azurerm_subnet_nat_gateway_association.cert_ci_jenkins_io_outbound_controller
+  to   = module.cert_ci_jenkins_io_outbound.azurerm_subnet_nat_gateway_association.outbound["cert-ci-jenkins-io-vnet-controller"]
+}
+moved {
+  from = azurerm_subnet_nat_gateway_association.cert_ci_jenkins_io_outbound_ephemeral_agents
+  to   = module.cert_ci_jenkins_io_outbound.azurerm_subnet_nat_gateway_association.outbound["cert-ci-jenkins-io-vnet-ephemeral-agents"]
+}
+module "cert_ci_jenkins_io_outbound" {
+  source = "./.shared-tools/terraform/modules/azure-nat-gateway"
+
   name                = "cert-ci-jenkins-io-outbound"
-  location            = azurerm_virtual_network.cert_ci_jenkins_io.location
   resource_group_name = azurerm_virtual_network.cert_ci_jenkins_io.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-resource "azurerm_nat_gateway" "cert_ci_jenkins_io_outbound" {
-  name                = "cert-ci-jenkins-io-outbound"
-  location            = azurerm_virtual_network.cert_ci_jenkins_io.location
-  resource_group_name = azurerm_virtual_network.cert_ci_jenkins_io.resource_group_name
-  sku_name            = "Standard"
-}
-resource "azurerm_nat_gateway_public_ip_association" "cert_ci_jenkins_io_outbound" {
-  nat_gateway_id       = azurerm_nat_gateway.cert_ci_jenkins_io_outbound.id
-  public_ip_address_id = azurerm_public_ip.cert_ci_jenkins_io_outbound.id
-}
-resource "azurerm_subnet_nat_gateway_association" "cert_ci_jenkins_io_outbound_controller" {
-  subnet_id      = azurerm_subnet.cert_ci_jenkins_io_controller.id
-  nat_gateway_id = azurerm_nat_gateway.cert_ci_jenkins_io_outbound.id
-}
-resource "azurerm_subnet_nat_gateway_association" "cert_ci_jenkins_io_outbound_ephemeral_agents" {
-  subnet_id      = azurerm_subnet.cert_ci_jenkins_io_ephemeral_agents.id
-  nat_gateway_id = azurerm_nat_gateway.cert_ci_jenkins_io_outbound.id
+  vnet_name           = azurerm_virtual_network.cert_ci_jenkins_io.name
+  subnet_names = [
+    azurerm_subnet.cert_ci_jenkins_io_controller.name,
+    azurerm_subnet.cert_ci_jenkins_io_ephemeral_agents.name,
+  ]
 }
 ####################################################################################
 ## NAT gateway to allow outbound connection on a centralized and scalable appliance
 ####################################################################################
-resource "azurerm_public_ip" "trusted_outbound" {
+moved {
+  from = azurerm_public_ip.trusted_outbound
+  to   = module.trusted_outbound.azurerm_public_ip.outbound
+}
+moved {
+  from = azurerm_nat_gateway.trusted_outbound
+  to   = module.trusted_outbound.azurerm_nat_gateway.outbound
+}
+moved {
+  from = azurerm_nat_gateway_public_ip_association.trusted_outbound
+  to   = module.trusted_outbound.azurerm_nat_gateway_public_ip_association.outbound
+}
+moved {
+  from = azurerm_subnet_nat_gateway_association.trusted_outbound_controller
+  to   = module.trusted_outbound.azurerm_subnet_nat_gateway_association.outbound["trusted-ci-jenkins-io-vnet-controller"]
+}
+moved {
+  from = azurerm_subnet_nat_gateway_association.trusted_outbound_ephemeral_agents
+  to   = module.trusted_outbound.azurerm_subnet_nat_gateway_association.outbound["trusted-ci-jenkins-io-vnet-ephemeral-agents"]
+}
+moved {
+  from = azurerm_subnet_nat_gateway_association.trusted_outbound_permanent_agents
+  to   = module.trusted_outbound.azurerm_subnet_nat_gateway_association.outbound["trusted-ci-jenkins-io-vnet-permanent-agents"]
+}
+module "trusted_outbound" {
+  source = "./.shared-tools/terraform/modules/azure-nat-gateway"
+
   name                = "trusted-outbound"
-  location            = azurerm_virtual_network.trusted_ci_jenkins_io.location
   resource_group_name = azurerm_virtual_network.trusted_ci_jenkins_io.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-resource "azurerm_nat_gateway" "trusted_outbound" {
-  name                = "trusted-outbound"
-  location            = azurerm_virtual_network.trusted_ci_jenkins_io.location
-  resource_group_name = azurerm_virtual_network.trusted_ci_jenkins_io.resource_group_name
-  sku_name            = "Standard"
-}
-resource "azurerm_nat_gateway_public_ip_association" "trusted_outbound" {
-  nat_gateway_id       = azurerm_nat_gateway.trusted_outbound.id
-  public_ip_address_id = azurerm_public_ip.trusted_outbound.id
-}
-resource "azurerm_subnet_nat_gateway_association" "trusted_outbound_controller" {
-  subnet_id      = azurerm_subnet.trusted_ci_jenkins_io_controller.id
-  nat_gateway_id = azurerm_nat_gateway.trusted_outbound.id
-}
-resource "azurerm_subnet_nat_gateway_association" "trusted_outbound_permanent_agents" {
-  subnet_id      = azurerm_subnet.trusted_ci_jenkins_io_permanent_agents.id
-  nat_gateway_id = azurerm_nat_gateway.trusted_outbound.id
-}
-resource "azurerm_subnet_nat_gateway_association" "trusted_outbound_ephemeral_agents" {
-  subnet_id      = azurerm_subnet.trusted_ci_jenkins_io_ephemeral_agents.id
-  nat_gateway_id = azurerm_nat_gateway.trusted_outbound.id
+  vnet_name           = azurerm_virtual_network.trusted_ci_jenkins_io.name
+  subnet_names = [
+    azurerm_subnet.trusted_ci_jenkins_io_controller.name,
+    azurerm_subnet.trusted_ci_jenkins_io_permanent_agents.name,
+    azurerm_subnet.trusted_ci_jenkins_io_ephemeral_agents.name,
+  ]
 }
