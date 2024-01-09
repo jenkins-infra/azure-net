fed66823d7bbc1339229bc87e5bd7977605e3a77
smerle33 <stephane.merle+jenkins@gmail.com>
2024-01-09 09:26:39 +0100

    chore(updatecli): add an updatecli manifest to update letsencrypt expiration date for trusted.ci and cert.ci

---
+++ updatecli/scripts/dateadd.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This script calculate next date for certificates
+##
+set -eux -o pipefail
+DATE_BIN='date'
+
+## non GNU operating system
+if command -v gdate >/dev/null 2>&1
+then
+    DATE_BIN='gdate'
+fi
+command -v "${DATE_BIN}" >/dev/null 2>&1 || { echo "ERROR: ${DATE_BIN} command not found. Exiting."; exit 1; }
+
+# "${DATE_BIN}" --utc +"%Y-%m-%dT00:00:00Z" -d '+1 month' #keep for later use with allinone not alpine based
+"${DATE_BIN}" -d@"$(( $(date +%s)+60*60*24*30*3))" --utc '+%Y-%m-%dT00:00:00Z' # next date in around 3 month
---
+++ updatecli/scripts/datediff.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# This script calculate diff between dates for letsencrypt expiration
+set -eux -o pipefail
+
+currentexpirydate="${1}"
+DATE_BIN='date'
+
+## non GNU operating system
+if command -v gdate >/dev/null 2>&1
+then
+    DATE_BIN='gdate'
+fi
+command -v "${DATE_BIN}" >/dev/null 2>&1 || { echo "ERROR: ${DATE_BIN} command not found. Exiting."; exit 1; }
+
+currentdateepoch=$("${DATE_BIN}" --utc "+%s" 2>/dev/null)
+if [ "${DATE_BIN}" = "gdate" ]
+then
+    expirydateepoch=$("${DATE_BIN}" "+%s" -d "$currentexpirydate")
+else
+    expirydateepoch=$("${DATE_BIN}" "+%s" -d "$currentexpirydate" -D "%Y-%m-%dT00:00:00Z")
+fi
+
+datediff=$(((expirydateepoch-currentdateepoch)/(60*60*24))) # diff per days
+
+if [ "$datediff" -lt 100 ] # launch renew 10 days before expiration
+then
+    echo "time for update"
+    exit 0
+else
+    echo "not yet expired"
+    exit 1
+fi
