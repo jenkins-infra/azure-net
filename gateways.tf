8f095dc06855a745af1dd41cdc6843c3873730f9
Damien Duportal <damien.duportal@gmail.com>
2024-01-17 18:22:37 +0100

    feat(gateways) add NAT gateways for ci.jenkins.io subnets (controller, agents and agents_sponsorship) (#194)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- gateways.tf
+++ gateways.tf
@@ -55,3 +55,32 @@ module "trusted_outbound_sponsorship" {
     azurerm_subnet.trusted_ci_jenkins_io_sponsorship_ephemeral_agents.name,
   ]
 }
+
+####################################################################################
+## NAT gateway to allow outbound connection on a centralized and scalable appliance
+####################################################################################
+module "ci_jenkins_io_outbound" {
+  source = "./.shared-tools/terraform/modules/azure-nat-gateway"
+
+  name                = "ci-jenkins-io-outbound"
+  resource_group_name = azurerm_virtual_network.public.resource_group_name
+  vnet_name           = azurerm_virtual_network.public.name
+  subnet_names = [
+    azurerm_subnet.public_vnet_ci_jenkins_io_controller.name,
+    azurerm_subnet.public_vnet_ci_jenkins_io_agents.name,
+  ]
+}
+module "ci_jenkins_io_outbound_sponsorship" {
+  source = "./.shared-tools/terraform/modules/azure-nat-gateway"
+
+  providers = {
+    azurerm = azurerm.jenkins-sponsorship
+  }
+
+  name                = "ci-jenkins-io-outbound-sponsorship"
+  resource_group_name = azurerm_virtual_network.public_jenkins_sponsorship.resource_group_name
+  vnet_name           = azurerm_virtual_network.public_jenkins_sponsorship.name
+  subnet_names = [
+    azurerm_subnet.public_jenkins_sponsorship_vnet_ci_jenkins_io_agents.name,
+  ]
+}
