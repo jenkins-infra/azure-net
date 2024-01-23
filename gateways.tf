9b9cbf31d75e586ef60d87726d9ca27b09e7a25d
Damien Duportal <damien.duportal@gmail.com>
2024-01-23 12:47:39 +0100

    feat(gateways) add NAT gateways for the publick8s and privatek8s clusters (#198)
    * feat(gateways) add NAT gateways for the publick8s and privatek8s clusters
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * wip: start with unassociated gateways
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    ---------
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- gateways.tf
+++ gateways.tf
@@ -84,3 +84,27 @@ module "ci_jenkins_io_outbound_sponsorship" {
     azurerm_subnet.public_jenkins_sponsorship_vnet_ci_jenkins_io_agents.name,
   ]
 }
+
+module "privatek8s_outbound" {
+  source = "./.shared-tools/terraform/modules/azure-nat-gateway"
+
+  name                = "privatek8s-outbound"
+  resource_group_name = azurerm_virtual_network.private.resource_group_name
+  vnet_name           = azurerm_virtual_network.private.name
+  subnet_names = [
+    ## Commented for phase 1 of https://github.com/jenkins-infra/helpdesk/issues/3908#issuecomment-1905856702
+    # azurerm_subnet.privatek8s_tier.name,
+  ]
+}
+
+module "publick8s_outbound" {
+  source = "./.shared-tools/terraform/modules/azure-nat-gateway"
+
+  name                = "publick8s-outbound"
+  resource_group_name = azurerm_virtual_network.public.resource_group_name
+  vnet_name           = azurerm_virtual_network.public.name
+  subnet_names = [
+    ## ## Commented for phase 1 of https://github.com/jenkins-infra/helpdesk/issues/3908#issuecomment-1905856702
+    # azurerm_subnet.publick8s_tier.name,
+  ]
+}
