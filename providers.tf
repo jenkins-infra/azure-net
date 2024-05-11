61599e11339a6454a7789c61e2b17f6a0c0dbbcd
Damien Duportal <damien.duportal@gmail.com>
2024-05-11 13:09:51 +0200

    hotfix: bump shared tools + explicit specification of main (CDF-paid) subscription id
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 # Configure the Microsoft Azure Providers
 provider "azurerm" {
+  subscription_id            = "dff2ec18-6a8e-405c-8e45-b7df7465acf0"
   skip_provider_registration = "true"
   features {}
 }
