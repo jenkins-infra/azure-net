8fbc260241a7e0b262114434604d3a2db0c15fd0
Damien Duportal <damien.duportal@gmail.com>
2024-06-06 16:07:58 +0200

    feat: add report publication from local file to automate jenkins-infra-data extraction
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,23 @@
-######################################################## Gateways Outbound IPs
-output "cert_ci_outbound_ips" {
-  value = module.cert_ci_jenkins_io_outbound.public_ip_list
+resource "local_file" "jenkins_infra_data_report" {
+  content = jsonencode({
+    "cert.ci.jenkins.io" = {
+      "outbound_ips" = concat(split(",", module.cert_ci_jenkins_io_outbound.public_ip_list), split(",", module.cert_ci_jenkins_io_outbound_sponsorship.public_ip_list)),
+    },
+    "trusted.ci.jenkins.io" = {
+      "outbound_ips" = concat(split(",", module.trusted_outbound.public_ip_list), split(",", module.trusted_outbound_sponsorship.public_ip_list)),
+    },
+    "ci.jenkins.io" = {
+      "outbound_ips" = concat(split(",", module.ci_jenkins_io_outbound.public_ip_list), split(",", module.ci_jenkins_io_outbound_sponsorship.public_ip_list)),
+    },
+    "infra.ci.jenkins.io" = {
+      "outbound_ips" = concat(split(",", module.infra_ci_outbound_sponsorship.public_ip_list), split(",", module.privatek8s_outbound.public_ip_list)),
+    },
+    "privatek8s.jenkins.io" = {
+      "outbound_ips" = split(",", module.publick8s_outbound.public_ip_list),
+    },
+    "publick8s.jenkins.io" = {
+      "outbound_ips" = split(",", module.publick8s_outbound.public_ip_list),
+    },
+  })
+  filename = "${path.module}/jenkins-infra-data-reports/azure-net.json"
 }
-output "cert_ci_sponsorship_outbound_ip_list" {
-  value = module.cert_ci_jenkins_io_outbound_sponsorship.public_ip_list
-}
-output "trusted_ci_outbound_ip_list" {
-  value = module.trusted_outbound.public_ip_list
-}
-output "trusted_ci_sponsorship_outbound_ip_list" {
-  value = module.trusted_outbound_sponsorship.public_ip_list
-}
-output "ci_outbound_ip_list" {
-  value = module.ci_jenkins_io_outbound.public_ip_list
-}
-output "ci_sponsorship_outbound_ip_list" {
-  value = module.ci_jenkins_io_outbound_sponsorship.public_ip_list
-}
-output "infra_ci_outbound_ip_list" {
-  value = module.infra_ci_outbound_sponsorship.public_ip_list
-}
-output "publick8s_outbound_ip_list" {
-  value = module.publick8s_outbound.public_ip_list
-}
-output "privatek8s_outbound_ip_list" {
-  value = module.privatek8s_outbound.public_ip_list
-}
-##############################################################################
