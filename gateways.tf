1739fd177696d562b69cd900ce9fed5c73177603
Damien Duportal <damien.duportal@gmail.com>
2024-08-01 11:10:01 +0200

    feat(gateways) increase amount of outbound IPs for ci.jenkins.io agents to spread egress workloads
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- gateways.tf
+++ gateways.tf
@@ -88,7 +88,7 @@ module "ci_jenkins_io_outbound_sponsorship" {
     module.public_sponsorship_vnet.subnets["public-jenkins-sponsorship-vnet-ci_jenkins_io_kubernetes"],
   ]
 
-  outbound_ip_count = 2
+  outbound_ip_count = 4
 }
 
 module "privatek8s_outbound" {
