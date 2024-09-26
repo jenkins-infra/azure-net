da6a453d120020b736b325b92c4c5f89867e3597
Damien Duportal <damien.duportal@gmail.com>
2024-09-26 09:05:24 +0200

    feat(DNS records) switch Update Center to Azure (#299)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- dns-records.tf
+++ dns-records.tf
@@ -79,7 +79,7 @@ resource "azurerm_dns_cname_record" "updates_jenkins_io" {
   zone_name           = data.azurerm_dns_zone.jenkinsio.name
   resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
   ttl                 = 60
-  record              = "aws.updates.jenkins.io"
+  record              = "azure.updates.jenkins.io"
 
   tags = merge(local.default_tags, {
     purpose = "Jenkins Update Center"
