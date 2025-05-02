8e3bfe1ec6eaaad0949962c3169176158eb73d07
Damien Duportal <damien.duportal@gmail.com>
2025-05-02 12:52:04 +0200

    feat(dns,updatecli) remove all Letsencrypt related resources (#377)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- dns.tf
+++ dns.tf
@@ -42,72 +42,19 @@ resource "azurerm_dns_ns_record" "do_jenkins_io" {
   tags = local.default_tags
 }
 
-resource "azurerm_dns_zone" "child_zones" {
-  for_each = local.lets_encrypt_dns_challenged_domains
+# Cleanup once the resources are managed in jenkins-infra/azure - https://github.com/jenkins-infra/helpdesk/issues/4630#issuecomment-2846886448
+removed {
+  from = azurerm_dns_zone.child_zones
 
-  name = each.key
-  # Use the same resource group for all DNS zones
-  resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
-
-  tags = local.default_tags
-}
-
-# create DNS record of type NS for child-zone in the parent zone (to allow propagation of DNS records)
-resource "azurerm_dns_ns_record" "child_zone_ns_records" {
-  for_each = local.lets_encrypt_dns_challenged_domains
-
-  name                = trimsuffix(each.key, ".jenkins.io") # only the flat name not the fqdn
-  zone_name           = data.azurerm_dns_zone.jenkinsio.name
-  resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
-  ttl                 = 60
-
-  records = azurerm_dns_zone.child_zones[each.key].name_servers
-
-  tags = local.default_tags
-}
-
-resource "azuread_application" "letsencrypt_dns_challenges" {
-  for_each = { for key, value in local.lets_encrypt_dns_challenged_domains : key => value if value != "" }
-
-  display_name = "letsencrypt-${each.key}"
-  owners       = [data.azuread_service_principal.terraform-azure-net-production.object_id]
-  tags         = [for key, value in local.default_tags : "${key}:${value}"]
-
-  required_resource_access {
-    resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
-
-    resource_access {
-      id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # User.Read
-      type = "Scope"
-    }
-  }
-
-  web {
-    homepage_url = "https://github.com/jenkins-infra/azure-net"
+  lifecycle {
+    destroy = false
   }
 }
+# Cleanup once the resources are managed in jenkins-infra/azure - https://github.com/jenkins-infra/helpdesk/issues/4630#issuecomment-2846886448
+removed {
+  from = azurerm_dns_ns_record.child_zone_ns_records
 
-resource "azuread_service_principal" "child_zone_service_principals" {
-  for_each = { for key, value in local.lets_encrypt_dns_challenged_domains : key => value if value != "" }
-
-  app_role_assignment_required = false
-  owners                       = [data.azuread_service_principal.terraform-azure-net-production.object_id]
-
-  client_id = azuread_application.letsencrypt_dns_challenges[each.key].client_id
-}
-
-resource "azuread_application_password" "child_zone_app_passwords" {
-  for_each = { for key, value in local.lets_encrypt_dns_challenged_domains : key => value if value != "" }
-
-  display_name   = "${each.key}-tf-managed"
-  application_id = azuread_application.letsencrypt_dns_challenges[each.key].id
-  end_date       = each.value
-}
-
-resource "azurerm_role_assignment" "child_zone_service_principal_assignements" {
-  for_each = { for key, value in local.lets_encrypt_dns_challenged_domains : key => value if value != "" }
-
-  scope                = azurerm_dns_zone.child_zones[each.key].id
-  role_definition_name = "DNS Zone Contributor" # Predefined standard role in Azure
-  principal_id         = azuread_service_principal.child_zone_service_principals[each.key].object_id
+  lifecycle {
+    destroy = false
+  }
 }
