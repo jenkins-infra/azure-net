db2c46611c1d985afe37c85825994501555de5c8
Alexander Brandes <mc.cache@web.de>
2025-07-09 22:32:24 +0200

    Add DNS record for JB sponsorship
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- dns-records.tf
+++ dns-records.tf
@@ -402,6 +402,19 @@ resource "azurerm_dns_txt_record" "apex_jenkinsciorg" {
   tags = local.default_tags
 }
 
+resource "azurerm_dns_txt_record" "jetbrains_sponsorship_domain_verification" {
+  name                = "@"
+  zone_name           = data.azurerm_dns_zone.jenkinsio.name
+  resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
+  ttl                 = 60
+
+  record {
+    value = "jetbrains-domain-verification=9memu05gp112di1q7gijggppf"
+  }
+
+  tags                = local.default_tags
+}
+
 ### CAA Records
 # CAA records to restrict Certificate Authorities
 resource "azurerm_dns_caa_record" "jenkins_caa" {
