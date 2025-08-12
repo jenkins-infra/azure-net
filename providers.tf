2802ffa720114c923e194be598926c442d1c5523
Damien Duportal <damien.duportal@gmail.com>
2025-08-12 09:42:47 +0200

    cleanup: remove jenkins-sponsorship provider (aka. sponsored subscription) (#425)
    * cleanup: remove all remnants of privatek8s-sponsorship
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup: remove jenkins-sponsorship provider (aka. sponsored subscription)
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    ---------
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- providers.tf
+++ providers.tf
@@ -4,9 +4,3 @@ provider "azurerm" {
   resource_provider_registrations = "none"
   features {}
 }
-provider "azurerm" {
-  alias                           = "jenkins-sponsorship"
-  subscription_id                 = "1311c09f-aee0-4d6c-99a4-392c2b543204"
-  resource_provider_registrations = "none"
-  features {}
-}
