ae814cc490909eda33106346efee2458ed4e8b4e
jayfranco999 <jaideepfrancis999@gmail.com>
2026-05-29 20:12:04 +0530

    feat(vpn): add jay_jenkins public IP for SSH access to private.vpn
    Signed-off-by: jayfranco999 <jaideepfrancis999@gmail.com>

--- locals.tf
+++ locals.tf
@@ -24,6 +24,7 @@ locals {
     dduportal = ["86.207.165.174"],
     mwaite    = ["162.142.59.220"],
     hlemeur   = ["82.67.38.76"],
+    jay_jenkins = ["106.51.242.101"],
   }
 
   # Tracked by updatecli, easier to use a string split as a list by Terraform
